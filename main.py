4f3ab947b5a45551a8f0efb10901a06f164cb0d5
HarryBerry87 <46969575+HarryBerry87@users.noreply.github.com>
2019-03-18 20:50:51 -0400

    ready for test

--- main.py
+++ main.py
@@ -5,13 +5,3 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
-
-
-@app.route("/about")
-def about():
-    return render_template('about.html')
-
-
-@app.route("/camera/<int:cam_id>")
-def album():
-    return render_template('album.html', cam_id=cam_id)
